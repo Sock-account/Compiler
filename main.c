4181efcab98ccb32892a7974aa7c785bdd047fa7
Sock-account <bromo.exe2@gmail.com>
2026-05-24 14:39:34 -0400

    The lexer can find parentheses and digits has as well as display multi-digit numbers correctly on the same line

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h>
 typedef enum {
     SEMI,
     OPEN_PAREN,
@@ -27,22 +28,53 @@ typedef struct {
 typedef struct {
     TokenTypeSeparator type;
 } TokenSeparator;
-
-int main() {
-    FILE *file;
-    file = fopen("C:\\Users\\Owner\\CLionProjects\\Compiler\\test.txt", "r");
+void lexer(FILE *file) {
     char current = fgetc(file);
-
+    char previous;
     if (file == NULL) {
         printf("Error reading file\n");
-        return 1;
+        return;
     }
     while (current != EOF) {
         if (current == ';') {
+            // Each if branch excluding the one for detecting if current is a digit
+            // has to check if previous is a digit so that multi-digit numbers display correctly on the same line.
+            if (isdigit(previous)) {
+                printf("\n");
+            }
             printf("FOUND SEMICOLON\n");
         }
+        else if (current == '(') {
+            if (isdigit(previous)) {
+                printf("\n");
+            }
+            printf("FOUND OPEN PAREN\n");
+        }
+        else if (current == ')') {
+            if (isdigit(previous)) {
+                printf("\n");
+            }
+            printf("FOUND CLOSE PAREN\n");
+
+        }
+        else if (isdigit(current)) {
+            if (isdigit(previous)) {
+                printf("%d", current - 48);
+            }
+            else {
+                printf("FOUND DIGIT: %d", current - 48);
+            }
+            }
+
         //printf("%c", current);
+        previous = current;
         current = fgetc(file);
     }
+}
+int main() {
+    FILE *file;
+    file = fopen("C:\\Users\\Owner\\CLionProjects\\Compiler\\test.txt", "r");
+    lexer(file);
+
 
 }
