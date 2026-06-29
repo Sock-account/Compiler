4460c3c643b0d16d388c9a5c65045869f43953ec
Sock-account <bromo.exe2@gmail.com>
2026-06-29 14:49:19 -0400

    Starting on a new file

--- test.c
+++ test.c
@@ -2,3 +2,19 @@
 // Created by Owner on 6/28/2026.
 //
 // So the guide on I have been using decided to start a new file entirely. Because the lexer wasn't reading the parentheses.
+#include <stdio.h>
+
+int main() {
+    FILE *file;
+    char buffer[100];
+
+    file = fopen("test.txt", "r");
+
+    fseek(file, 0, SEEK_SET);
+
+    fread(buffer, 1, 1, file);
+    printf("%c\n", buffer);
+    fclose(file);
+
+    return 0;
+}
